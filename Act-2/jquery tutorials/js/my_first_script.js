6ae7f4b034163525c537e12429d8a1f077d93cd8
jill <jillian_bear@outlook.com>
2019-10-29 21:00:42 -0400

    add cursor files

---
+++ Act-2/jquery tutorials/js/my_first_script.js
@@ -0,0 +1,164 @@
+$(document).mousemove(function(movementInfo){
+
+	
+
+
+
+	var mX=movementInfo.clientX; //THE X POSITION OF THE MOUSE ON SCREEN
+	var  mY=movementInfo.clientY; // THE Y POSITION
+	
+
+
+	$("#cc4j").css({ //THIS FOLLOWS THE MOUSE.
+
+		"top": mY,
+		"left": mX
+	})
+
+	$("#cc4j2").css({
+
+		"top": $(window).height()-mY, //the total body height-the Y position
+		"left": mX
+	})
+
+	
+	$("#cc4j3").css({
+
+		"right":mX,
+		"top": mY
+	})
+
+
+	$("#cc4j4").css({
+
+		"right":mX,
+		"top": $(window).height()-mY
+	})
+
+	$("#cc4j5").css({
+
+		"left":$(window).width()-mX,
+		"top": $(window).height()/2
+	})
+
+	$("#cc4j7").css({
+
+		"right":$(window).width()-mX,
+		"top": $(window).height()/2
+	})
+
+	$("#cc4j6").css({
+
+		"right":mY,
+		"top": mY
+	})
+
+	$("#cc4jC").css({
+
+		"left":mY,
+		"top": mY
+
+	})
+
+	$("#cc4jU").css({
+
+		"bottom":mY,
+		"right": mY
+
+	})
+
+	
+
+	$("#cc4j8").css({
+
+		"left": mY,
+		"bottom": mY
+	})
+
+$("#cc4j9").css({
+
+		"left":$(window).height()-mY,
+		"top": $(window).width()/3.6
+
+		
+	})
+
+$("#cc4j10").css({
+
+		"right":$(window).height()-mY,
+		"bottom": $(window).width()/4.1
+
+		
+	})
+
+$("#cc4j11").css({
+
+		"right": $(window).height()+mX,
+		"bottom": $(window).width()/4.1
+	})
+
+$("#cc4j12").css({
+
+		"right": $(window).width()/2,
+		"bottom": mY
+	})
+
+
+$("#cc4j13").css({
+
+		"left": $(window).height()+mX,
+		"bottom": $(window).width()/4.1
+	})
+
+
+$("#cc4j14").css({
+
+		"right": $(window).width()/2,
+		"top": mY
+	})
+		
+	})
+
+
+$(document).click(function(){
+	$(".cursor").toggleClass("rotate")
+
+
+
+	
+})
+
+var $win = $(window),
+    w = 0,h = 0,
+    rgb = [],
+    getWidth = function() {
+        w = $win.width();
+        h = $win.height();
+    };
+
+$win.resize(getWidth).mousemove(function(e) {
+
+    rgb = [
+        Math.round(e.pageX/w * 255),
+        Math.round(e.pageY/h * 255),
+        150
+    ];
+
+    $(document.body).css('background','rgb('+rgb.join(',')+')');
+
+}).resize();
+
+
+
+
+
+
+
+$("body").css({
+	"cursor":"none"
+})
+
+
+
+
+
